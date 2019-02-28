b61b8317fdea3d145d0c59e4a59e4cee85ae83b6
Martin Blomberg <martin.blomberg@outlook.com>
2019-02-28 16:25:20 +0100

    Initial commit

---
+++ src/main/java/se/iths/martin/apisample/ApisampleApplication.java
@@ -0,0 +1,13 @@
+package se.iths.martin.apisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApisampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/martin/apisample/ApisampleApplicationTests.java
@@ -0,0 +1,16 @@
+package se.iths.martin.apisample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApisampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
