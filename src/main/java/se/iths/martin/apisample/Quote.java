3c5384c81be1a7fa2c67ed446c53d378c8bf2bd1
Martin Blomberg <martin.blomberg@outlook.com>
2019-03-05 22:14:00 +0100

    Changed to manual constructors in Quote

--- src/main/java/se/iths/martin/apisample/Quote.java
+++ src/main/java/se/iths/martin/apisample/Quote.java
@@ -9,14 +9,21 @@ import org.springframework.lang.NonNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
-@NoArgsConstructor
-@RequiredArgsConstructor
 public class Quote {
 
     private @Id @GeneratedValue Long id;
     private @NonNull String quote;
     private java.time.LocalDateTime dateTime;
+
+    public Quote() {
+    }
+
+    public Quote(String quote, LocalDateTime dateTime) {
+        this.quote = quote;
+        this.dateTime = dateTime;
+    }
 }
