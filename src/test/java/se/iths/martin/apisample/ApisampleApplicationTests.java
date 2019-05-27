a42c8adcd596a0787388f7a91b5c89ecfd3a0491
Martin Blomberg <martin.blomberg@outlook.com>
2019-05-27 10:47:50 +0200

    Added fail test

--- src/test/java/se/iths/martin/apisample/ApisampleApplicationTests.java
+++ src/test/java/se/iths/martin/apisample/ApisampleApplicationTests.java
@@ -1,9 +1,12 @@
 package se.iths.martin.apisample;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertFalse;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,11 @@ public class ApisampleApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void fail(){
+        assertFalse(true);
     }
 
 }
